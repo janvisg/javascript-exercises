8e0f6d706c50d3790db1ae1ccea4e2b6f89b409b
janvisg <janvisruthi.com>
2024-03-01 13:37:25 -0600

    edit sumAll check edge cases and sum in the range

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(num1,num2) {
+    let res=0;
+    if(!Number.isInteger(num1) || !Number.isInteger(num2)){
+        return "ERROR";
+    }
+    if(num1<0 || num2<0){
+        return "ERROR";
+    }
+    if(num1>num2){
+        let temp=num1;
+        num1=num2;
+        num2=temp;
+    }
+    for(let i=num1;i<=num2;i++){
+        res+=i;
+    }
+    return res;
 };
 
 // Do not edit below this line
